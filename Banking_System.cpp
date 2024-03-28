4fc635f9544fef67de580aef2f0b507e4b10252f
Brian_Velazquez <148260206+BrianVelazquez5776@users.noreply.github.com>
2024-03-28 17:05:58 -0400

    Create Banking_System.cpp

---
+++ Banking_System.cpp
@@ -0,0 +1,258 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+#include<cctype>
+#include<iomanip>
+
+
+using namespace std;
+
+class Bank
+        {
+public:
+   char name[40];
+   char type[40];
+    int pin, account_number;
+   float balance;
+    void new_account();
+    void show_account() const;
+    void modify_account();
+    void report() const;
+    void deposit(int);
+    void withdraw(int);
+    int return_accountnum() const;
+};
+
+
+void Bank::new_account()
+{
+    srand(static_cast<unsigned int>(time(nullptr)));
+    account_number = rand() % 900000 + 100000;
+    cout << "\nYour Account Number: #" << account_number;
+    cout << "\n\n Enter Your Name: ";
+    cin.ignore();
+    cin.getline(name,40);
+    cout << "\nEnter the type of the account (Checkings/Savings): ";
+    cin >> type;
+    cout << "\nEnter a pin for your account {5 digits}: ";
+    cin >> pin;
+    cout << "\nEnter a deposit: ";
+    cin >> balance;
+    cout << "\n Account was created successfully.";
+}
+
+void Bank::modify_account()
+{
+    cout << "\n\n Enter Your New Name: ";
+    cin.ignore();
+    cin.getline(name,40);
+    cout << "\n Enter the new type of the account (Checkings/Savings): ";
+    cin >> type;
+    cout << "Enter a new pin for your account {5 digits}: ";
+    cin >> pin;
+
+}
+
+void Bank::show_account() const
+{
+    cout<<"\nAccount No. : #" << account_number;
+    cout<<"\nAccount Holder Name : " << name;
+    cout<<"\nType of Account : "<< type;
+    cout<<"\nBalance amount : $" << balance;
+}
+
+void Bank::report() const
+{
+    cout << account_number << setw(10) << " " << name << setw(10) << " " << type << setw(10) << " " << balance << endl;
+}
+
+void Bank::deposit(int n)
+{
+    balance += n;
+}
+
+void Bank::withdraw(int n)
+{
+    balance -= n;
+}
+
+int Bank::return_accountnum() const
+{
+    return account_number;
+}
+
+void create_account(); // creates a new account
+void change_account(int); // modifies a specific user's account
+void display_account(int); // displays a specific account
+void delete_account(int);
+void display_all_accounts();
+
+
+void create_account()
+{
+    Bank obj;
+    ofstream file;
+    file.open("newdatabase.dat", ios::binary | ios::app);
+    obj.new_account();
+    file.write(reinterpret_cast<char *> (&obj), sizeof(Bank));
+    file.close();
+}
+
+void change_account(int n)
+{
+    Bank obj;
+    bool flag = false;
+    fstream file;
+    file.open("newdatabase.dat", ios::binary | ios::in | ios::out);
+    if(!file)
+    {
+        cout << "File Could Not Be Opened.";
+        return;
+    }
+    cout <<"\nModifying Account\n";
+    while(!file.eof() && flag == false)
+    {
+        file.read(reinterpret_cast<char*> (&obj), sizeof(Bank));
+        if(obj.return_accountnum() == n)
+        {
+            obj.show_account();
+            cout << "\nPlease enter your new details" << endl;
+            obj.modify_account();
+            int position = (-1) * static_cast<int>(sizeof(Bank)); // calculates the position where the object "obj" should be rewritten in the file "database". static_case<int> converts it into a integer.
+            file.seekp(position, ios::cur); // this moves the file pointer by the value of position relative to the current position which is ios::cur.
+            file.write(reinterpret_cast<char *> (&obj), sizeof(Bank)); // this is better over creating a copy of the file, deleting the original and renaming the copy to the original repeatedly.
+            cout << "\nAccount was successfully modified.";
+            flag = true;
+        }
+    }
+    file.close();
+    if(flag == false)
+    {
+        cout << "\nDatabase was not found." << endl;
+    }
+}
+
+void delete_account(int n)
+{
+    Bank obj;
+    ifstream file; // input file stream object, used to read data from the file
+    ofstream file2; // output file stream , used to write data into the file
+    file.open("newdatabase.dat", ios::binary);
+    if(!file)
+    {
+        cout << "File Does Not Exist.";
+        return;
+    }
+    file2.open("temp_newdatabase.dat", ios::binary); // creates a temp file that used to write
+    file.seekg(0, ios::beg); // moves the file pointer to the beginning of the file
+    while(file.read(reinterpret_cast<char *> (&obj), sizeof(Bank)))
+    {
+        if(obj.return_accountnum() != n)
+        {
+            file2.write(reinterpret_cast<char *> (&obj), sizeof(Bank));
+        }
+    }
+    file.close();
+    file2.close();
+    remove("newdatabase.dat");
+    rename("temp_newdatabase.dat", "newdatabase.dat");
+    cout << "Account was successfully deleted.";
+}
+
+
+void display_account(int n)
+{
+    Bank obj;
+    bool flag = false;
+    ifstream file;
+    file.open("newdatabase.dat", ios::binary);
+    if(!file)
+    {
+        cout << "File Could Not Be Opened.";
+        return;
+    }
+
+    cout << "\nAccount Details\n";
+    while(file.read(reinterpret_cast<char *> (&obj), sizeof(Bank)))
+    {
+        if(obj.return_accountnum() == n)
+        {
+            obj.show_account();
+            flag = true;
+        }
+    }
+    file.close();
+    if(flag == false)
+    {
+        cout << "\n\nAccount Number does not exist";
+    }
+}
+
+void display_all_accounts()
+{
+    Bank obj;
+    ifstream file;
+    file.open("newdatabase.dat", ios::binary);
+    if(!file)
+    {
+        cout << "File Could Not Be Opened";
+        return;
+    }
+    cout<<"\n\n\t\tACCOUNT HOLDER LIST\n\n";
+    cout<<"==================================================================================\n";
+    cout<<"A/c no.          NAME            Type            Balance\n";
+    cout<<"==================================================================================\n";
+    while(file.read(reinterpret_cast<char *> (&obj), sizeof(Bank)))
+    {
+        obj.report();
+    }
+    file.close();
+}
+
+int main()
+{
+    int choice, n;
+    do
+    {
+        cout << "\n ATM Management System";
+        cout << "\n 1. New User";
+        cout << "\n 2. Account Details";
+        cout << "\n 3. Modify User Details";
+        cout << "\n 4. Delete Account";
+        cout << "\n 5. Show All Accounts";
+        cout << "\n Enter Your Choice : ";
+        cin >> choice;
+        switch(choice)
+        {
+            case 1:
+                create_account();
+                break;
+            case 2:
+                cout << "Please enter you account number: #";
+                cin >> n;
+                display_account(n);
+                break;
+            case 3:
+                cout << "Please enter your account number: #";
+                cin >> n;
+                change_account(n);
+                break;
+            case 4:
+                cout << "Please enter your account number: #";
+                cin >> n;
+                delete_account(n);
+                break;
+            case 5:
+                display_all_accounts();
+                break;
+            case 10:
+                cout << "Thanks for your time. ";
+            default:
+                cout << "Error: Invalid input. Try again.";
+        }
+        cin.ignore(); // Clear input buffer
+        cin.get(); // Wait for user to press enter
+    }while(choice != 10);
+    return 0;
+}
